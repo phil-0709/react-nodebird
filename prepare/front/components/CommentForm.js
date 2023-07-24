724ac2c900e4f1d9c38d5415f3b8206a03c23628
cpk0709 <cpk0709@gmail.com>
2023-07-24 23:35:09 +0900

    댓글 버튼 추가

--- prepare/front/components/CommentForm.js
+++ prepare/front/components/CommentForm.js
@@ -1,15 +1,24 @@
-import { Form } from 'antd';
+import { Form, Input, Button } from 'antd';
 import { useCallback } from 'react';
-import { useInput } from '../hooks/useInput';
+import useInput from '../hooks/useInput';
 
 const CommentForm = (props) => {
   const { post } = props;
-  const [commentText, setCommentText] = useInput('');
+  const [commentText, onChangeCommentText] = useInput('');
   const onSubmitComment = useCallback(() => {
     console.log(post);
   }, [post, commentText]);
 
-  return <Form onFinish={onSubmitComment}>폼</Form>;
+  return (
+    <Form onFinish={onSubmitComment}>
+      <Form.Item>
+        <Input.TextArea value={commentText} onChange={onChangeCommentText} />
+        <Button type='primary' htmlType='submit'>
+          삐약
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default CommentForm;
