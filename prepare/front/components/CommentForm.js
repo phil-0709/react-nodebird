d908f3bad8de8991d4166c14b3d6743c996ca3c3
cpk0709 <cpk0709@gmail.com>
2023-07-24 23:54:56 +0900

    리덕스에서 아이디 가져오기

--- prepare/front/components/CommentForm.js
+++ prepare/front/components/CommentForm.js
@@ -2,12 +2,15 @@ import { Form, Input, Button } from 'antd';
 import { useCallback } from 'react';
 import useInput from '../hooks/useInput';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 const CommentForm = (props) => {
   const { post } = props;
+  const id = useSelector((state) => state.user.me?.id);
   const [commentText, onChangeCommentText] = useInput('');
   const onSubmitComment = useCallback(() => {
     console.log(post);
+    console.log(post.id, commentText);
   }, [post, commentText]);
 
   return (
