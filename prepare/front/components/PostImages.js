c82d2a0287f01001801635401a572c284181da18
cpk0709 <cpk0709@gmail.com>
2023-08-01 00:02:12 +0900

    .

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -4,11 +4,52 @@ const PostImages = ({ images }) => {
   if (images.length === 1) {
     return (
       <>
-        <img src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <img
+          role='presentation'
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
       </>
     );
   }
-  return <div>준비중 입니다...</div>;
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role='presentation'
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role='presentation'
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+  return (
+    <div>
+      <img
+        role='presentation'
+        src={images[0].src}
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+      <div
+        role='presentation'
+        style={{
+          display: 'inline-block',
+          width: '50%',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+        }}
+      ></div>
+    </div>
+  );
 };
 
 PostImages.PropTypes = {
